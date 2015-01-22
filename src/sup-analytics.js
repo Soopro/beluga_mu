7e84ae5722f20fabc1832f1d78a82b8eda18531a
redy <redy.ru@gmail.com>
2015-01-23 03:47:57 +0800

    xxx

--- src/sup-analytics.js
+++ src/sup-analytics.js
@@ -11,23 +11,23 @@ Version 1.0.0
 */
 
 (function supAnalytics() {
-    'use strict';
-	var API_HOST = 'http://localhost:5000/'
-    if(!window['sa']){
-        return;
-    }
+  'use strict';
+  var API_HOST = 'http://api.soopro.com/'
+  if(!window['sa']){
+      return;
+  }
 
-    var xmlhttp;
-    var api=API_HOST+'ws/'+window['sa']+'/visit';
+  var xmlhttp;
+  var api=window['sa_api']+'ws/'+window['sa']+'/visit';
 
-    if (window.XMLHttpRequest) {
-        // code for IE7+, Firefox, Chrome, Opera, Safari
-        xmlhttp = new XMLHttpRequest();
-    } else {
-        // code for IE6, IE5
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-    }
+  if (window.XMLHttpRequest) {
+      // code for IE7+, Firefox, Chrome, Opera, Safari
+      xmlhttp = new XMLHttpRequest();
+  } else {
+      // code for IE6, IE5
+      xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  }
 
-    xmlhttp.open("GET", api, true);
-    xmlhttp.send();
+  xmlhttp.open("GET", api, true);
+  xmlhttp.send();
 })();
